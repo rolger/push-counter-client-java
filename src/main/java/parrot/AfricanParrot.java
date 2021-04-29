f861966689d5f8466a563ba9b16b1d4ef928e96c
michaelraulicpecher <michael.raulic-pecher@nagarro.com>
2021-04-29 17:04:37 +0200

    removed dead code from AfricanParrot

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -9,15 +9,6 @@ public class AfricanParrot extends Parrot {
 
   @Override
   public double getSpeed() {
-      switch (type) {
-          case EUROPEAN:
-              return getBaseSpeed();
-          case AFRICAN:
-              return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-          case NORWEGIAN_BLUE:
-              return (isNailed) ? 0 : getBaseSpeed(voltage);
-          default:
-              throw new RuntimeException("Should be unreachable");
-      }
+    return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
   }
 }
