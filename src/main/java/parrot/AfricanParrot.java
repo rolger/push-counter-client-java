d2c0241436fa004e73cce50a10bc5f7453a80c6f
michaelraulicpecher <michael.raulic-pecher@nagarro.com>
2021-04-29 16:53:05 +0200

    extraced AfricanParrot

---
+++ src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,9 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+
+  public AfricanParrot(int numberOfCoconuts, double voltage,
+      boolean isNailed) {
+    super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+  }
+}
