a1a483faadd54fb389b5610989e3c31986a1178e
michaelraulicpecher <michael.raulic-pecher@nagarro.com>
2021-04-29 17:05:49 +0200

    removed dead code from NorwegianBlueParrot

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -9,15 +9,6 @@ public class NorwegianBlueParrot extends Parrot {
 
   @Override
   public double getSpeed() {
-      switch (type) {
-          case EUROPEAN:
-              return getBaseSpeed();
-          case AFRICAN:
-              return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-          case NORWEGIAN_BLUE:
-              return (isNailed) ? 0 : getBaseSpeed(voltage);
-          default:
-              throw new RuntimeException("Should be unreachable");
-      }
+    return (isNailed) ? 0 : getBaseSpeed(voltage);
   }
 }
