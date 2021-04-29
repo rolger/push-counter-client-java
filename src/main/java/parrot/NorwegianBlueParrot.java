da174e804193c8ace00c35fab4549be2d1cdf314
michaelraulicpecher <michael.raulic-pecher@nagarro.com>
2021-04-29 16:54:13 +0200

    extraced NorwegianBlueParrot

---
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,9 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot {
+
+  public NorwegianBlueParrot(int numberOfCoconuts, double voltage,
+      boolean isNailed) {
+    super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
+  }
+}
