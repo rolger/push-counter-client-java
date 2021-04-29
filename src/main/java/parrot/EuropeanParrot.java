11dc0efc20451176e2523c41573663c6e244fe44
michaelraulicpecher <michael.raulic-pecher@nagarro.com>
2021-04-29 16:49:27 +0200

    extracted EuropeanParrot

---
+++ src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,9 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot {
+
+  public EuropeanParrot(int numberOfCoconuts, double voltage,
+      boolean isNailed) {
+    super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+  }
+}
